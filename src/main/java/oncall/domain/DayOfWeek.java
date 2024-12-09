d0e18aa36525895bfaca33bf0d02414861ea55f2
이창근 <dlckdrms0517@gmail.com>
2024-12-09 21:55:20 +0900

    feat: 법정공휴일 도메인 작성

--- src/main/java/oncall/domain/DayOfWeek.java
+++ src/main/java/oncall/domain/DayOfWeek.java
@@ -21,4 +21,13 @@ public enum DayOfWeek {
 				.findFirst()
 				.orElseThrow(CustomExceptions.ILLEGAL_ARGUMENT::get);
 	}
+	
+	public static boolean isWeekday(int targetDay, DayOfWeek monthStartDayOfWeek) {
+		DayOfWeek dayOfWeek = monthStartDayOfWeek.add(targetDay % 7);
+		return dayOfWeek != 토 && dayOfWeek != 일;
+	}
+	
+	private DayOfWeek add(int day) {
+		return DayOfWeek.values()[this.ordinal() + day];
+	}
 }
---
+++ src/main/java/oncall/domain/LegalHoliday.java
@@ -0,0 +1,35 @@
+package oncall.domain;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum LegalHoliday {
+	
+	신정(1, 1),
+	삼일절(3, 1),
+	어린이날(5, 5),
+	현충일(6, 6),
+	광복절(8, 15),
+	개천절(10, 3),
+	한글날(10, 9),
+	성탄절(12, 25),
+	;
+	
+	private final int month;
+	private final int day;
+	
+	LegalHoliday(int month, int day) {
+		this.month = month;
+		this.day = day;
+	}
+	
+	public boolean isHolidayOnWeekDay(int month, int day, DayOfWeek monthStartDayOfWeek) {
+		Optional<LegalHoliday> holiday = Arrays.stream(LegalHoliday.values())
+				.filter(legalHoliday -> legalHoliday.month == month && legalHoliday.day == day)
+				.findFirst();
+		if (holiday.isEmpty()) {
+			return false;
+		}
+		return DayOfWeek.isWeekday(day, monthStartDayOfWeek);
+	}
+}
