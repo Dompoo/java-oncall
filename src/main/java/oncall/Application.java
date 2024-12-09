6d35e791b425e5319c0ed50c768428adfdcc68cc
이창근 <dlckdrms0517@gmail.com>
2024-12-09 23:35:09 +0900

    feat: Application 작성

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,34 @@
 package oncall;
 
+import oncall.common.RetryHandler;
+import oncall.common.exception.ExceptionHandler;
+import oncall.controller.OncallController;
+import oncall.io.input.InputHandler;
+import oncall.io.input.InputParser;
+import oncall.io.input.InputValidator;
+import oncall.io.output.OutputHandler;
+import oncall.io.output.OutputParser;
+import oncall.io.reader.ConsoleReader;
+import oncall.io.writer.ConsoleWriter;
+import oncall.io.writer.Writer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Writer writer = new ConsoleWriter();
+        InputHandler inputHandler = new InputHandler(
+                new ConsoleReader(),
+                writer,
+                new InputValidator(),
+                new InputParser()
+        );
+        OutputHandler outputHandler = new OutputHandler(
+                writer,
+                new OutputParser()
+        );
+        new OncallController(
+                inputHandler,
+                outputHandler,
+                new RetryHandler(new ExceptionHandler(writer))
+        ).run();
     }
 }
