d4b15fa202619c89801a939d0f42fee83d6c3d93
이창근 <dlckdrms0517@gmail.com>
2024-12-13 00:34:18 +0900

    fix: 근무 변경 로직 수정
    - 기존에는 '다음'근무자와 교체했지만, 이제는 평일과 휴일을 따져서 교체합니다.
    - 평일 연속 근무는 다음 평일 근무자와 변경
    - 휴일 연속 근무는 다음 휴일 근무자와 변경

--- src/main/java/oncall/domain/OncallPlanner.java
+++ src/main/java/oncall/domain/OncallPlanner.java
@@ -50,23 +50,50 @@ public class OncallPlanner {
 		return new OncallPlanResult(oncallCalendar.getMonthValue(), oncallDayPlans);
 	}
 	
+	/*
+	평일-휴일 연속이면 다음 휴일 근무자와 교체
+	휴일-평일 연속이면 다음 평일 근무자와 교체
+	 */
 	public static void handleShift(List<OncallDayPlan> oncallDayPlans) {
 		String prevEmergencyWorkerName = "";
 		for (int i = 0; i < oncallDayPlans.size(); i++) {
 			String currEmergencyWorkerName = oncallDayPlans.get(i).emergencyWorkerName();
 			if (prevEmergencyWorkerName.equals(currEmergencyWorkerName)) {
-				switchCurrentWorkerAndNextWorker(oncallDayPlans, i);
+				if (oncallDayPlans.get(i).calendarDay().isWeekDay()) {
+					//다음 평일 근무자와 교체
+					switchWithNextWeekDayWorker(oncallDayPlans, i);
+				}
+				//다음 휴일 근무자와 교체
+				switchWithNextHolidayWorker(oncallDayPlans, i);
 			}
 			prevEmergencyWorkerName = oncallDayPlans.get(i).emergencyWorkerName();
 		}
 	}
 	
-	private static void switchCurrentWorkerAndNextWorker(List<OncallDayPlan> oncallDayPlans, int currentIndex) {
-		String currEmergencyWorkerName = oncallDayPlans.get(currentIndex).emergencyWorkerName();
-		String nextEmergencyWorkerName = oncallDayPlans.get(currentIndex + 1).emergencyWorkerName();
-		OncallDayPlan currPlan = oncallDayPlans.remove(currentIndex).emergencyWorkerNameOf(nextEmergencyWorkerName);
-		OncallDayPlan nextPlan = oncallDayPlans.remove(currentIndex).emergencyWorkerNameOf(currEmergencyWorkerName);
-		oncallDayPlans.add(currentIndex, nextPlan);
-		oncallDayPlans.add(currentIndex, currPlan);
+	private static void switchWithNextHolidayWorker(List<OncallDayPlan> oncallDayPlans, int curr) {
+		for (int i = curr + 1; i < oncallDayPlans.size(); i++) {
+			if (!oncallDayPlans.get(i).calendarDay().isWeekDay()) {
+				switchWork(oncallDayPlans, curr, i);
+				return;
+			}
+		}
+	}
+	
+	private static void switchWithNextWeekDayWorker(List<OncallDayPlan> oncallDayPlans, int curr) {
+		for (int i = curr + 1; i < oncallDayPlans.size(); i++) {
+			if (oncallDayPlans.get(i).calendarDay().isWeekDay()) {
+				switchWork(oncallDayPlans, curr, i);
+				return;
+			}
+		}
+	}
+	
+	private static void switchWork(List<OncallDayPlan> oncallDayPlans, int curr, int next) {
+		String currEmergencyWorkerName = oncallDayPlans.get(curr).emergencyWorkerName();
+		String nextEmergencyWorkerName = oncallDayPlans.get(next).emergencyWorkerName();
+		OncallDayPlan newNextPlan = oncallDayPlans.remove(next).emergencyWorkerNameOf(currEmergencyWorkerName);
+		OncallDayPlan newCurrPlan = oncallDayPlans.remove(curr).emergencyWorkerNameOf(nextEmergencyWorkerName);
+		oncallDayPlans.add(next, newNextPlan);
+		oncallDayPlans.add(curr, newCurrPlan);
 	}
 }
